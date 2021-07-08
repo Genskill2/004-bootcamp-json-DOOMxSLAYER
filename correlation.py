6fba77b71ebb73e931ed0e582aa0585db25e02c3
DOOMxSLAYER <84665718+DOOMxSLAYER@users.noreply.github.com>
2021-07-09 00:30:55 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,1 +1,19 @@
+import json
+
 # Add the functions in this file
+def load_function(filename):
+    f= open(filename)
+    data= json.load(f) 
+    f.close()
+    return data
+    
+def compute_phi(file,eventname):
+    y=load_function('journal.json')
+    n1p=0
+    n0p=0
+    np1=0
+    np0=0
+    n11=0
+    n00=0
+    n10=0
+    n01=0
